ecd4e0418496a1f312e4adaabc37af1991d0f1bb
EC2 Default User <ec2-user@ip-172-31-94-108.ec2.internal>
2021-12-18 06:24:32 +0000

    [Finish]3-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    book = Book.new(book_params)
+    book.save
+    redirect_to blog_path(blog.id)
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book), notice: 'successfully' #詳細表示へ
+    else
+      @books = Book.all
+      render action: :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'successfully' #詳細画面へ
+      else
+        @books = Book.all
+        render action: :edit
+      end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
